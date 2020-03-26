b7f4649976ad09a6bed4006a0222ec6313e97697
jonitt <jonit44@gmail.com>
2020-03-27 00:56:05 +0800

    Setting up

---
+++ src/containers/app/App.js
@@ -0,0 +1,13 @@
+//Author: Joni Tuhkanen
+//this is the main app
+
+import React, { Component } from 'react';
+import styles from './App.css';
+
+class App extends Component {
+  render() {
+    return <div className={styles.app}></div>;
+  }
+}
+
+export default App;
