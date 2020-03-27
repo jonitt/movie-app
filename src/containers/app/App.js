727ad305bae414511d880c63de6266181d17001c
jonitt <jonit44@gmail.com>
2020-03-28 01:29:23 +0800

    File setups

--- src/containers/app/App.js
+++ src/containers/app/App.js
@@ -2,11 +2,16 @@
 //this is the main app
 
 import React, { Component } from 'react';
-import styles from './App.css';
+import styles from './App.module.css';
+import MovieSearch from '../movie-search/MovieSearch';
 
 class App extends Component {
   render() {
-    return <div className={styles.app}></div>;
+    return (
+      <div className={styles.app}>
+        <MovieSearch />
+      </div>
+    );
   }
 }
 
---
+++ src/containers/movie-search/MovieSearch.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import styles from './MovieSearch.module.css';
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.root}>
+      </div>
+    );
+  }
+}
+
+export default App;
