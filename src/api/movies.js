6b01785a24b8dfd4a20763b302ecbb655e81a39e
jonitt <jonit44@gmail.com>
2020-04-04 13:58:03 +0800

    Changed fetch address to https

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,7 +1,7 @@
 import { key } from '../apikey';
 
 export const fetchMovie = (name, year) =>
-  fetch(`http://www.omdbapi.com/?t=${name}&apikey=${key}&type=movie&y=${year}`)
+  fetch(`https://www.omdbapi.com/?t=${name}&apikey=${key}&type=movie&y=${year}`)
     .then(res => res.json())
     .catch(err => {
       console.log(err);
