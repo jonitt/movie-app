2bbf65e3be362bcb47bdd538566de1d64bbde08a
jonitt <jonit44@gmail.com>
2020-04-04 13:46:26 +0800

    remove build from gitignore

---
+++ build/precache-manifest.1cf62f4ae6a431b59a1842c0360afffb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b722da0cd4241fe1bc39d85c723188",
+    "url": "/index.html"
+  },
+  {
+    "revision": "408c153cc00c27b42550",
+    "url": "/static/css/main.7d414948.chunk.css"
+  },
+  {
+    "revision": "3d68cdb911844e8fd0db",
+    "url": "/static/js/2.dca10452.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dca10452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408c153cc00c27b42550",
+    "url": "/static/js/main.d423343c.chunk.js"
+  },
+  {
+    "revision": "9b4741d2110818558093",
+    "url": "/static/js/runtime-main.2a485af8.js"
+  }
+]);
